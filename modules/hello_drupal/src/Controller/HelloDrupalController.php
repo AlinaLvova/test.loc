0a0a48f03edfcf0bde3d6f0a1f12ca3d27bfa163
Lvova Alina <lvova.aline@gmail.com>
2023-11-03 01:09:27 +0400

    feat: add access check

--- modules/hello_drupal/src/Controller/HelloDrupalController.php
+++ modules/hello_drupal/src/Controller/HelloDrupalController.php
@@ -4,16 +4,19 @@ namespace Drupal\hello_drupal\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Controller for the Hello Drupal module.
  */
-class HelloDrupalController extends ControllerBase {
+class HelloDrupalController extends ControllerBase
+{
 
   /**
    * Display a greeting message.
    */
-  public function hello() {
+  public function hello()
+  {
     return [
       '#title' => $this->t("I'm a Drupal Developer!"),
     ];
@@ -22,17 +25,31 @@ class HelloDrupalController extends ControllerBase {
   /**
    * Display a personalized greeting for the current user.
    */
-  public function helloMe() {
+  public function helloMe()
+  {
     $current_user = \Drupal::currentUser();
 
-    return [
-      '#title' => $this->t("Hello, @username (@uid)!", [
-        '@username' => $current_user->getDisplayName(),
-        '@uid' => $current_user->id(),
-      ]),
-      '#markup' => $this->t("Current server time: @time", [
-        '@time' => \Drupal::service('date.formatter')->format(\Drupal::time()->getRequestTime(), 'custom', 'Y-m-d H:i:s'),
-      ]),
-    ];
+    if ($current_user->isAuthenticated() || $current_user->hasPermission('administer site configuration')) {
+
+      return [
+        '#title' => $this->t("Hello, @username (@uid)!", [
+          '@username' => $current_user->getDisplayName(),
+          '@uid' => $current_user->id(),
+        ]),
+        '#markup' => $this->t("Current server time: @time", [
+          '@time' => \Drupal::service('date.formatter')->format(\Drupal::time()->getRequestTime(), 'custom', 'Y-m-d H:i:s'),
+        ]),
+      ];
+    } else {
+      // Handle access denied.
+      return $this->accessDenied();
+    }
+  }
+  /**
+   * Custom access denied page.
+   */
+  public function accessDenied()
+  {
+    return new Response($this->t('Access Denied'), 403);
   }
 }
